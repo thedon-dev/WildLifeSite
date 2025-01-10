749d2c2cb3dd14b052d9c87bb17d9537702a1b48
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-01-10 06:56:40 +0100

    NavLink fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="px-[5%] lg:px-[10%] py-10">
       <div className="flex justify-between 2xl:container mx-auto items-center">
-        <Link to="/home" className="text-3xl whitespace-nowrap">
+        <Link to="/" className="text-3xl whitespace-nowrap">
           Jacob <span className="text-[#4343a9]">WildLife</span>
         </Link>
         <div className="border-t-4 border-black md:border-0 flex justify-around md:justify-end py-3 lg:py-0 gap-5 fixed bottom-0 md:relative ms-auto z-10 bg-white left-0 w-full">
@@ -21,7 +21,7 @@ const Navbar = () => {
             <Link
               to="/"
               className={`py-2 px-4 ${
-                isActiveLink("/home") ? "text-blue-500" : "text-black"
+                isActiveLink("/") ? "text-blue-500" : "text-black"
               }`}
             >
               <p className="hidden md:flex">Home</p>
